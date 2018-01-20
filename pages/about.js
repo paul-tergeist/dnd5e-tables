813962c95be19eafb2e5009cbd64e9ea245dfb8b
paul-tergeist <github@muenschke.de>
2018-01-20 10:06:25 +0100

    initial commit
    while following the tutorial from learnnextjs.com

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Header from '../comps/Header'
+
+const About = () => (
+  <div>
+    <Header />
+    <p>About me</p>
+  </div>
+
+)
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../comps/Header'
+
+const Index = () => (
+  <div>
+    <Header />
+    <p>Hello World</p>
+  </div>
+)
+
+export default Index
